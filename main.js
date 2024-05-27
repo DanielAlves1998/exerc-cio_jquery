873c9464b6b18567e08fd6dfaa7f4ef752366c37
Daniel Alves <daniel199831.da@gmail.com>
2024-05-27 20:07:34 -0300

    commit

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     //adicionar tarefa
     $('#addTarefaBtn').on('click', function() {
         var tarefaText = $('#tarefaInput').val();
-        
-        if (tarefaText.lenghth > 0) addTarefa(tarefaText)
+        if (tarefaText.length > 0) addTarefa(tarefaText)
+        $('#tarefaInput').val('');
     });
 
     //função para adicionar tarefa
     function addTarefa(text) {
         $('#tarefaList').append('<li><span>&times;</span>' + text + '</li>');
     }
+
+    //marcar/desmarcar tarefa como concluida
+    $(document).on('click', 'li', function () {
+        $(this).toggleClass('completed');
+    });
+
+//remover tarefa
+    $(document).on('click', 'span', function (e) {
+        e.stopPropagation();  //evitar a propagação do evento para o elemento pai (li)
+        $(this).parent().fadeOut(500, function () {
+            $(this).remove();
+        });
+    });
 })
